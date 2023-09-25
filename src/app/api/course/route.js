60b7c922abee8306f6a268e0ed921cdc0cb95c30
Kitpakorn <kitpakorn.chopper@gmail.com>
2023-09-25 17:00:35 +0700

    Edited

--- src/app/api/course/route.js
+++ src/app/api/course/route.js
@@ -4,6 +4,5 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   const prisma = getPrisma();
   const courses = await prisma.course.findMany();
-
-  return NextResponse.json({ ok: true, courses: courses });
+  return NextResponse.json({ ok: true, courses });
 };
